6db6638dff013e1b45257f3ec3fcec29fcf79678
Fnsf3 <83078056+Fnsf3@users.noreply.github.com>
2026-01-06 13:02:22 +0100

    LCD Manager V1

---
+++ src/lcdManager.h
@@ -0,0 +1,14 @@
+
+
+
+// 
+
+#include "fuck-arduino-kekw/Arduino-LiquidCrystal-I2C-library/LiquidCrystal_I2C.h"
+#include "MainStateMachine.h"
+#include "Util.h"
+
+
+void lcdInit();
+void lcdUpdate (struct Task* myTask);
+
+
