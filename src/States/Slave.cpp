f782ad02d4c0276853a443068f7bd7669f8416db
kletskid <ad.gerritsen2@student.han.nl>
2026-01-13 15:35:35 +0100

    sorry, another typo

--- src/States/Slave.cpp
+++ src/States/Slave.cpp
@@ -5,8 +5,8 @@ void slaveState(struct State* currState){ //TODO: fix this horrible piece of shi
     setMotorState(FORWARDS);
   }
   else if (
-      (lineSensors[0] && lineSensors[2] || lineSensors[3])
-      (lineSensors[1] && lineSensors[2] || lineSensors[3])
+      (lineSensors[0] && (lineSensors[2] || lineSensors[3])) ||
+      (lineSensors[1] && (lineSensors[2] || lineSensors[3]))
       ) {
 	  setMotorState(FORWARDS);
   }
