223ea3506ef702798a91b2e8b6c51806d9f60554
Genarro <Genarrokoorndijk@hotmail.com>
2026-01-20 11:57:45 +0100

    Remote control change
    switch case qua code indeling

--- src/States/RemoteControl.cpp
+++ src/States/RemoteControl.cpp
@@ -1,22 +1,44 @@
 #include "RemoteControl.h"
 
-void remoteControlState(struct State* currState){
-  if (currState->currentEvent == InputComplete){
-    if (strncmp(inputBuffer[0], "w", 1) == 0){
-      setMotorMode(FORWARDS);
-    } else if (strncmp(inputBuffer[0], "a", 1) == 0) {
-      setMotorMode(LEFT);
-    } else if (strncmp(inputBuffer[0], "d", 1) == 0) {
-      setMotorMode(RIGHT);
-    } else if (strncmp(inputBuffer[0], "s", 1) == 0) {
-      setMotorMode(BACKWARDS);     
-    } else if (strncmp(inputBuffer[0], "halt", 4) == 0){
-      setMotorMode(HALTED);
-    } else {
-      prints("unkown command\r\n");
-    }
-    flushArguments();
-  }
-}
+void remoteControlState(struct State* currState) {
 
+    if (currState->currentEvent == InputComplete) {
+
+        char cmd = inputBuffer[0][0]; // eerste karakter
+
+        switch (cmd) {
+
+            case 'w':
+                setMotorMode(FORWARDS);
+                break;
+
+            case 'a':
+                setMotorMode(LEFT);
+                break;
+
+            case 'd':
+                setMotorMode(RIGHT);
+                break;
+
+            case 's':
+                setMotorMode(BACKWARDS);
+                break;
+
+            case 'h':
+                // volledige check voor "halt"
+                if (strncmp(inputBuffer[0], "halt", 4) == 0) {
+                    setMotorMode(HALTED);
+                } else {
+                    prints("unknown command\r\n");
+                }
+                break;
+
+            default:
+                prints("unknown command\r\n");
+                break;
+        }
+
+        flushArguments();
+    }
+}
 
