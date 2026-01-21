d5dcb9cbeb8429f4ffe5e404a280a97dc5158265
imbluedabr <baldergroen@gmail.com>
2026-01-21 13:04:43 +0100

    added power to lcd

--- src/lcdManager.cpp
+++ src/lcdManager.cpp
@@ -1,6 +1,6 @@
 #include "lcdManager.h"
 #include "InputManager.h"
-
+#include "Util.h"
 
 // Modus aangeven van waar die in zit
 // Rij richting
@@ -27,13 +27,27 @@ struct Task lcdTriggerTask = {
   };
 
 
-void lcdInit() {
-    lcd.preInit();
-    lcd.init();
-    lcd.backlight();
-    registerTask(&lcdTriggerTask); 
-    tick_delay (MS_TO_TICKS(1000));
+void lcdStoreTime() {
+  cli();
+  uint8_t* word = (uint8_t*) &ticks;
+  for (uint8_t i = 0; i < 8; i++) {
+    eepromWrite(EEPROM_TIME_ADRES + i, word[i]);
+  }
+  sei();
+}
 
+
+void lcdInit() {
+  lcd.preInit();
+  lcd.init();
+  lcd.backlight();
+  registerTask(&lcdTriggerTask); 
+  tick_delay (MS_TO_TICKS(1000));
+  uint8_t word[8];
+  for (uint8_t i = 0; i < 8; i++) {
+    word[i] = eepromRead(EEPROM_TIME_ADRES + i);
+  }
+  memcpy((void*) &ticks, word, 8);
 }
 
 void lcdPrint(char* str) {
@@ -43,25 +57,33 @@ void lcdPrint(char* str) {
 }
 
 void lcdUpdate (struct Task* myTask) {
-    int remainder = TICKS_IN_SECONDS % 3600;
-	int minutes = remainder / 60;
-	remainder = remainder % 60;
-	int hours = TICKS_IN_SECONDS / 3600;
-    char lcdBuffer [17] = "xx:xx:xx        ";
-    lcd.setCursor(0, 0);
-    BCDConvert(lcdBuffer, hours);
-    BCDConvert(lcdBuffer + 3, minutes);
-	BCDConvert(lcdBuffer + 6, remainder);
+  int remainder = TICKS_IN_SECONDS % 3600;
+  int minutes = remainder / 60;
+  remainder = remainder % 60;
+  int hours = TICKS_IN_SECONDS / 3600;
+  char lcdBuffer [17] = "xx:xx:xx        ";
+  lcd.setCursor(0, 0);
+  BCDConvert(lcdBuffer, hours);
+  BCDConvert(lcdBuffer + 3, minutes);
+  BCDConvert(lcdBuffer + 6, remainder);
 
-    const char *str = lcdRichting[currentMotorMode];
-    memcpy(lcdBuffer + 9, (char*)str, strnlen(str, 16));
-    lcdPrint(lcdBuffer);
+  const char *str = lcdRichting[currentMotorMode];
+  memcpy(lcdBuffer + 9, (char*)str, strnlen(str, 16));
+  lcdPrint(lcdBuffer);
 
-    lcd.setCursor(0, 1);
-    str = StringStates[currentState.id];
-	char stateBuffer[17] = "                ";
-    memcpy(&stateBuffer, (char*) str, strnlen(str, 16));
-    lcdPrint(stateBuffer);
-
+  lcd.setCursor(0, 1);
+  str = StringStates[currentState.id];
+  char stateBuffer[17] = "          xx%   ";
+  memcpy(&stateBuffer, (char*) str, strnlen(str, 16));
+  
+  float spd = ((float)motorBaseSpeed)/255;
+  uint16_t intspd = (uint16_t)(spd*100);
+  const char* fullspd = "100%";
+  if (intspd == 100) {
+    memcpy(stateBuffer + 10, (void*) fullspd, 4);
+  } else {
+    BCDConvert(stateBuffer + 10, intspd);
+  }
+  lcdPrint(stateBuffer);
 }
 
--- src/lcdManager.h
+++ src/lcdManager.h
@@ -1,16 +1,14 @@
-
-
-
-// 
+#pragma once
 
 #include "fuck-arduino-kekw/Arduino-LiquidCrystal-I2C-library/LiquidCrystal_I2C.h"
 #include "MainStateMachine.h"
 #include "Util.h"
 #include "Drivers/MotorDriver.h"
 
+#define EEPROM_TIME_ADRES 0
 
 void lcdInit();
 void lcdUpdate (struct Task* myTask);
 
-
+void lcdStoreTime();
 
